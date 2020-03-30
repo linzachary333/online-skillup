242157242d0d7e65460364a19ce911defda97247
Lin Zachary <linzachary@team-lab.com>
2020-03-30 09:50:32 +0900

    Implement vue-youtube

--- src/javascripts/components/MessageLog.vue
+++ src/javascripts/components/MessageLog.vue
@@ -9,11 +9,15 @@
       >
         <v-card-title>{{message.name}}</v-card-title>
         <v-card-subtitle>{{message.time}}</v-card-subtitle>
-          <v-card-text
-            class="cardText"
-          >
-            {{message.text}}
-          </v-card-text>
+        <v-card-text class="cardText">
+          {{message.text}}
+        </v-card-text>
+        <youtube
+          width="100%"
+          height="50%"
+          :video-id="videoId"
+          :player-vars="playerVars"
+        ></youtube>
       </v-card>
     </template>
   </ul>
@@ -26,6 +30,14 @@ export default {
   props: {
     messages: VueTypes.array.isRequired,
     userId: VueTypes.string.isRequired,
+  },
+  data() {
+    return {
+      videoId: 'lG0Ys-2d4MA',
+      playerVars: {
+        autoplay: 0
+      }
+    };
   },
   computed: {
     checkMessageId: function(messageId) {
--- src/javascripts/entry.js
+++ src/javascripts/entry.js
@@ -3,6 +3,9 @@ import '../css/base.scss';
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from '../plugins/vuetify';
+import VueYoutube from 'vue-youtube';
+
+Vue.use(VueYoutube);
 
 new Vue({
   vuetify,
