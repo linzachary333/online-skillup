52c4ef6e66ec3ab5e7f6559e2e6be8ff065d844d
Lin Zachary <linzachary@team-lab.com>
2020-03-30 15:02:56 +0900

    Add video functionality

--- src/javascripts/components/MessageLog.vue
+++ src/javascripts/components/MessageLog.vue
@@ -12,12 +12,17 @@
         <v-card-text class="cardText">
           {{message.text}}
         </v-card-text>
-        <youtube
-          width="100%"
-          height="50%"
-          :video-id="videoId"
-          :player-vars="playerVars"
-        ></youtube>
+        <template v-if="containsVideo(message.text)">
+          <youtube
+            v-for="(id, i) in videoIds(message.text)"
+            :key="i"
+            :video-id="id"
+            :player-vars="playerVars"
+            width="100%"
+            height="50%"
+          >
+          </youtube>
+        </template>
       </v-card>
     </template>
   </ul>
@@ -33,18 +38,35 @@ export default {
   },
   data() {
     return {
-      videoId: 'lG0Ys-2d4MA',
       playerVars: {
         autoplay: 0
-      }
+      },
     };
+  },
+  methods: {
+    containsVideo(text) {
+      const matches = this.parseYoutubeLink(text);
+      return matches !== null;
+    },
+    videoIds: function(text) {
+      const matches = this.parseYoutubeLink(text);
+      const idList = [];
+      for (const match of matches) {
+        idList.push(match.substring(match.length - 11));
+      }
+      return idList;
+    },
+    parseYoutubeLink(text) {
+      const regex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi;
+      return text.match(regex);
+    }
   },
   computed: {
     checkMessageId: function(messageId) {
       return {
         yourMessage: messageId === this.$props.userId
       };
-    }
+    },
   }
 };
 </script>
